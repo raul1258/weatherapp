4a39f25f7ec5db20e0a41d70a24ccccaac2eca88
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-02-07 21:46:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-50">
+     <h1>Weather App</h1>
+     <div class="input-group mb-3">
+    <input type="text" className="form-control" placeholder="Enter Place Name " aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+     <button class="input-group-text" id="basic-addon2">Search</button>
+</div>
+
     </div>
   );
 }
