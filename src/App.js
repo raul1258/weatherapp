b0283621c11bdb417e0e387f5e40cddd2ec3c932
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-02-08 21:57:11 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App w-50">
+    <div className="App">
      <h1>Weather App</h1>
      <div class="input-group mb-3">
     <input type="text" className="form-control" placeholder="Enter Place Name " aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-     <button class="input-group-text" id="basic-addon2">Search</button>
+     <Button variant="primary" id="basic-addon2">Search</Button>
 </div>
+
+
 
     </div>
   );
